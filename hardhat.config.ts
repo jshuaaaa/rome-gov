ae701aa5b1809f25829e5f8b03b4caa9484ae3cb
jshuaaaa <95507083+jshuaaaa@users.noreply.github.com>
2022-11-27 12:14:56 -0500

    feat: init

---
+++ hardhat.config.ts
@@ -0,0 +1,135 @@
+import 'dotenv/config';
+import '@nomiclabs/hardhat-waffle';
+import '@nomiclabs/hardhat-ethers';
+import '@nomiclabs/hardhat-etherscan';
+import '@typechain/hardhat';
+import '@typechain/hardhat/dist/type-extensions';
+import { removeConsoleLog } from 'hardhat-preprocessor';
+import 'hardhat-deploy';
+import { HardhatUserConfig, MultiSolcUserConfig, NetworksUserConfig } from 'hardhat/types';
+import 'tsconfig-paths/register';
+import 'hardhat-gas-reporter';
+
+const networks: NetworksUserConfig = true
+  ? {}
+  : {
+      hardhat: {
+        forking: {
+          enabled: false,
+          url: process.env.RPC_GOERLI as string,
+        },
+      },
+      goerli: {
+        url: process.env.RPC_GOERLI,
+        accounts: [process.env.PRIVATE_KEY_GOERLI as string],
+      },
+    };
+
+const config: HardhatUserConfig = {
+  defaultNetwork: 'hardhat',
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+  },
+  mocha: {
+    timeout: process.env.MOCHA_TIMEOUT || 300000,
+  },
+  gasReporter: {
+    enabled: true,
+    outputFile: 'gas-report.txt',
+    noColors: true,
+    currency: 'USD',
+    // coinmarketcap: process.env.COINMARKETCAP
+  },
+  networks,
+  solidity: {
+    compilers: [
+      {
+        version: '0.6.12',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 800,
+          },
+          metadata: {
+            // do not include the metadata hash, since this is machine dependent
+            // and we want all generated code to be deterministic
+            // https://docs.soliditylang.org/en/v0.7.6/metadata.html
+            bytecodeHash: 'none',
+          },
+        },
+      },
+      {
+        version: '0.7.5',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 800,
+          },
+          metadata: {
+            // do not include the metadata hash, since this is machine dependent
+            // and we want all generated code to be deterministic
+            // https://docs.soliditylang.org/en/v0.7.6/metadata.html
+            bytecodeHash: 'none',
+          },
+        },
+      },
+      {
+        version: '0.8.10',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 800,
+          },
+          metadata: {
+            // do not include the metadata hash, since this is machine dependent
+            // and we want all generated code to be deterministic
+            // https://docs.soliditylang.org/en/v0.7.6/metadata.html
+            bytecodeHash: 'none',
+          },
+        },
+      },
+      {
+        version: '0.8.17',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 800,
+          },
+          metadata: {
+            // do not include the metadata hash, since this is machine dependent
+            // and we want all generated code to be deterministic
+            // https://docs.soliditylang.org/en/v0.7.6/metadata.html
+            bytecodeHash: 'none',
+          },
+        },
+      },
+    ],
+  },
+  preprocess: {
+    eachLine: removeConsoleLog((hre) => hre.network.name !== 'hardhat'),
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  typechain: {
+    outDir: 'typechained',
+    target: 'ethers-v5',
+  },
+};
+
+if (process.env.TEST) {
+  (config.solidity as MultiSolcUserConfig).compilers = (config.solidity as MultiSolcUserConfig).compilers.map((compiler) => {
+    return {
+      ...compiler,
+      outputSelection: {
+        '*': {
+          '*': ['storageLayout'],
+        },
+      },
+    };
+  });
+}
+
+export default config;
